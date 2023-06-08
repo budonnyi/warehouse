8b534920188dfa66f505c213249973c3bb97ba33
Dmytro Budonnyi <budonnyi@gmail.com>
2023-06-08 21:42:24 +0300

    start

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+    'supportEmail' => 'robot@devreadwrite.com'
+];
+
